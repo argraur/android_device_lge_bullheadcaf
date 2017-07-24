93d8387de7b9cc3ed1ff8732a286cffd6eb10a1f
argraur <argraur@gmail.com>
2017-07-24 23:33:50 +0000

    Add XOS_bullhead-userdebug combo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 The Android Open Source Project
+# Copyright (C) 2017 The halogenOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo aosp_bullhead-userdebug
+add_lunch_combo XOS_bullhead-userdebug
