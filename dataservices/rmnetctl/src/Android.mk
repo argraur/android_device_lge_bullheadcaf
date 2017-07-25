0d6d9d95d62db3c8ee4a27bfab73cab31151ddef
argraur <argraur@gmail.com>
2017-07-25 07:17:29 +0000

    Add kernel dependencies for rmnetctl

--- dataservices/rmnetctl/src/Android.mk
+++ dataservices/rmnetctl/src/Android.mk
@@ -1,14 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+common_deps := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+kernel_includes := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+kernel_includes_uapi := $(kernel_includes)/uapi
+
 LOCAL_COPY_HEADERS_TO   := dataservices/rmnetctl
 LOCAL_COPY_HEADERS      := ../inc/librmnetctl.h
 
 LOCAL_SRC_FILES := librmnetctl.c
 LOCAL_CFLAGS := -Wall -Werror
 
+LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
+
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../inc
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_C_INCLUDES += $(kernel_includes)
+LOCAL_C_INCLUDES += $(kernel_includes_uapi)
 
 LOCAL_MODULE := librmnetctl
 LOCAL_MODULE_TAGS := optional
